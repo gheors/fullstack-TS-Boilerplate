7af3c80f769f5cc8be97dcd97805fafab293f772
sergio <gheors@usi.ch>
2021-11-03 15:00:56 +0100

    controller setup

---
+++ backend/src/controller/eventsControl.ts
@@ -0,0 +1,32 @@
+import { IEvent, Event } from "../model/event"
+
+
+// add Event to mongodb
+export async function addEvent(name: string) {
+    return new Promise((resolve, reject) => {
+        const event = new Event({ name })
+        console.log(event)
+        event.save().then(resolve).catch(reject)
+    })
+}
+
+// get all events from mongodb
+export async function getEvents(): Promise<IEvent[]> {
+    return new Promise<IEvent[]>((resolve, reject) => {
+        Event.find({}, (err, events) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(events)
+            }
+        })
+    })
+}
+
+
+// delete event from mongodb
+export async function deleteEvent(name: string) {
+    return new Promise((resolve, reject) => {
+        Event.deleteMany({ name: name }).then(resolve).catch(reject)
+    })
+}
