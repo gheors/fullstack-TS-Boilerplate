3a655086534bcf27be3a6357fdce69ec47196570
sergio <gheors@usi.ch>
2021-11-10 13:13:16 +0100

    Create FooterTitle.tsx

---
+++ frontend/components/footer/FooterTitle.tsx
@@ -0,0 +1,12 @@
+import { Box, VStack } from '@chakra-ui/layout'
+import React from 'react'
+
+export default function FooterTitle() {
+    return (
+        <VStack flexGrow={1} spacing={10}>
+            <Box flexGrow={1}>social</Box>
+            <Box flexGrow={1}>social</Box>
+            <Box flexGrow={1}>social</Box>
+        </VStack>
+    )
+}
